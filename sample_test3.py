6edd5e088b9de5539b589195dac1868cbf997591
Carlos <carvermac@gmail.com>
2026-06-18 13:58:15 -0400

    Agrego archivo de pruebas de pytest

---
+++ sample_test3.py
@@ -0,0 +1,12 @@
+import pytest
+def test_contains_five():
+    """
+    Tests the 'contains_five' function.
+    """
+    # Example list for testing
+    my_list = [1, 3, 5, 7, 9]
+    if contains_five(my_list)==True:
+        print("Excelent!!")
+   # assert contains_five(my_list) == True # Write your Python assert below
+
+print ("todo ok")
